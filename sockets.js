72e6696e30f2c5ff5958e0ad11209f4abe882455
jobb-rodriguez <jobb.rodriguez22@gmail.com>
2022-06-04 14:29:58 +0800

    Add rooms and fixed number to guess variable

--- sockets.js
+++ sockets.js
@@ -1,31 +1,34 @@
 let readyPlayerCount = 0;
-let numberToGuess = -1;
+let numberToGuess = {};
 
 function listen(io) {
     io.on('connection', (socket) => {
+        let room = 'room' + Math.floor(readyPlayerCount / 2);
         console.log(`a user connected: ${socket.id}`)
 
         socket.on('ready', () => {
+            socket.join(room);
             console.log(`Player ready: ${socket.id}`);
+
+            readyPlayerCount++;
+            if (readyPlayerCount % 2 == 0) {
+                numberToGuess[room] = Math.floor(Math.random() * 1001);
+                console.log(numberToGuess[room]);
+                io.in(room).emit('startGame');
+            }
         })
-        readyPlayerCount++;
-        if (readyPlayerCount % 2 == 0) {
-            numberToGuess = Math.floor(Math.random() * 1001);
-            console.log(numberToGuess);
-            io.emit('startGame');
-        }
 
         socket.on('guessNumber', (guess) => {
-            console.log('guess received', guess, numberToGuess);
+            console.log('guess received', guess, numberToGuess[room]);
             // -1: lower; 0: higher; 1: correct
             let status = -1;
-            if (guess === numberToGuess) {
+            if (guess === numberToGuess[room]) {
                 status = 1;
-            } else if (guess < numberToGuess) {
+            } else if (guess < numberToGuess[room]) {
                 status = 0;
             }
             if (status === 1) {
-                io.emit('correctGuess', socket.id);
+                io.in(room).emit('correctGuess', socket.id);
             } else {
                 io.to(socket.id).emit('incorrectGuess', status);
             }
@@ -33,6 +36,7 @@ function listen(io) {
 
         socket.on('disconnect', (reason) => {
             console.log(`${socket.id} disconnected due to ${reason}`);
+            socket.leave(room);
         })
     });
 }
